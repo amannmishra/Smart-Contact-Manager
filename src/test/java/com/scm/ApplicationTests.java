55630650385c086f5469bfaf139df69605fd3a6b
Aman Mishra <amanmishra3496@gmail.com>
2025-07-07 10:47:26 +0530

    Added Smart Contact Manager project

---
+++ src/test/java/com/scm/ApplicationTests.java
@@ -0,0 +1,24 @@
+package com.scm;
+
+import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+// import com.scm.services.EmailService;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+	// @Autowired
+	// private EmailService service;
+	
+	// @Test
+	// void sendEmailTest() {
+	// 	service.sendEmail("amanmishra3496@gmail.com", "just testing email service", "this is scm project working on email service");
+	// }
+
+}
